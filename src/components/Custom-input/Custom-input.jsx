ce540e1b75c5a7997eb40b4f898034ca399547af
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-05-15 09:33:08 -0300

    fix: add value to CustomInput component

--- src/components/Custom-input/Custom-input.jsx
+++ src/components/Custom-input/Custom-input.jsx
@@ -5,7 +5,12 @@ import "./Custom-input.scss";
 const CustomInput = ({ label, value, onChange }) => {
   return (
     <div className="custom-input-container">
-      <input type="" className="custom-input" onChange={(e) => onChange(e)} />
+      <input
+        type=""
+        className="custom-input"
+        value={value}
+        onChange={(e) => onChange(e)}
+      />
       {label ? (
         <label
           className={`${value.length > 0 ? "shrink" : ""} custom-input-label`}
