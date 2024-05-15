d03b2715c4855bde360d636e71285931717d7855
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-05-15 09:55:55 -0300

    feat: add function delete a task

--- src/components/Task-item/Task-item.jsx
+++ src/components/Task-item/Task-item.jsx
@@ -2,8 +2,19 @@ import PropTypes from "prop-types";
 
 import "./Task-item.scss";
 import { AiFillDelete } from "react-icons/ai";
+import axios from "axios";
 
 const TaskItem = ({ task }) => {
+  const handleTaskDeletion = async () => {
+    try {
+      await axios.delete(
+        `https://task-manager-backend-gc5o.onrender.com/tasks/${task._id}`
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="task-item-container">
       <div className="task-description">
@@ -23,7 +34,7 @@ const TaskItem = ({ task }) => {
       </div>
 
       <div className="delete">
-        <AiFillDelete size={18} color="#f97474" />
+        <AiFillDelete onClick={handleTaskDeletion} size={18} color="#f97474" />
       </div>
     </div>
   );
@@ -33,6 +44,7 @@ TaskItem.propTypes = {
   task: PropTypes.shape({
     description: PropTypes.string.isRequired,
     isCompleted: PropTypes.bool.isRequired,
+    _id: PropTypes.string.isRequired,
   }).isRequired,
 };
 
