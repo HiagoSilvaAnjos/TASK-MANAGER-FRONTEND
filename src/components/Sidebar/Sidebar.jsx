3981a410f4fe5174343bf82c11630a9664bd4cfc
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-05-16 09:38:02 -0300

    feat: add navigation to Login page in Home component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,13 +2,20 @@ import "./Sidebar.scss";
 import logo from "../../assets/images/1111.png";
 
 import CustomButton from "../Custom-button/Custom-button";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
+  const handleSingOutClick = () => {
+    navigate("/");
+  };
+
   return (
     <div className="sidebar-container">
       <img src={logo} alt="logo full stack club" />
       <div className="sing-out">
-        <CustomButton>Sair</CustomButton>
+        <CustomButton onClick={handleSingOutClick}>Sair</CustomButton>
       </div>
     </div>
   );
