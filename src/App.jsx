b9f95e5e8fe932c2825e194a8db42b7602e868a1
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-05-16 08:37:54 -0300

    feat: add app container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import "./App.scss";
+
+import Sidebar from "./components/Sidebar/Sidebar";
 import Tasks from "./components/Tasks/Tasks";
 
 function App() {
   return (
-    <>
+    <div className="app-container">
+      <Sidebar />
       <Tasks />
-    </>
+    </div>
   );
 }
 
